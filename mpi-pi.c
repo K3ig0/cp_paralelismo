05b303e360011e2ce87e5f9bc1619c03872c5e45
pablo <pablolopezviqueira@gmail.com>
2014-03-27 19:03:13 +0100

    Terminada salvo por falta de precisión, el problema está en el cálculo de h (probar a pasar n)

--- mpi-pi.c
+++ mpi-pi.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
     int i, n;
     double PI25DT = 3.141592653589793238462643;
-    double pi, h, sum, x;
+    double pi, h, sum, x, sum1;
 
     int numprocs, my_id;
     MPI_Status status;
@@ -25,29 +25,41 @@ int main(int argc, char *argv[])
 
     printf("my_id %d numprocs %d\n", my_id, numprocs);
 
-    if (numprocs > 1) {
-
-        if (my_id == 0) {
-            printf("Enter the number of intervals: (0 quits) \n");
-            scanf("%d",&n);
-        }
-        while (1)
-        {
+    if (numprocs > 1) { //Empieza código paralelizado
+        while (1) {
+            if (my_id == 0) {
+                printf("Enter the number of intervals: (0 quits) \n");
+                scanf("%d",&n);
+                for (i=1; i < numprocs; i++){
+                    MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                }
+            }
+            else {
+                MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+            }
             if (n == 0) break;
             
-
+            h   = 1.0 / (double) n;
+            sum = 0.0;
+            for (i = my_id; i < n; i+=numprocs) {
+                x = h * ((double)i - 0.5);
+                sum += 4.0 / (1.0 + x*x);
+            }
+            if (my_id != 0) {
+                sum1 = sum;
+                MPI_Send(&sum1, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            }
             else {
-
-
-
-                //codigo
-
-
-
+                for (i=1; i < numprocs; i++){
+                    MPI_Recv(&sum1, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+                    sum += sum1;
+                }
+                pi = h * sum;
+                printf("pi is approximately %.16f, Error is %.16f\n",
+                        pi, fabs(pi - PI25DT));
             }
         }
     }
-
     // Solo un proceso
     else {
         while (1) {
@@ -68,7 +80,5 @@ int main(int argc, char *argv[])
             printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
         }
     }
-
     MPI_Finalize();
-
 }
